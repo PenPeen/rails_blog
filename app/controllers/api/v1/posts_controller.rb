0fc11c00507d1fbd28f52433c5a8ee405d274ce7
Pen Peen <ikegami1022.h@gmail.com>
2025-04-11 08:26:21 +0900

    refactor(posts): 不要なページネーションメタデータメソッドを削除

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,29 +5,19 @@ module Api
 
       def index
         posts =
-          if params[:user_id].present?
+          if params[:user_id]
             Post.publish_posts_for_user(params[:user_id])
           else
             Post.all_published_posts
           end
 
         paginated_posts = posts.page(current_page).per(PER_PAGE)
-        render json: paginated_posts, meta: pagination_meta(paginated_posts)
+        render json: paginated_posts
       end
 
       private
         def current_page
           params[:page] || 1
-        end
-
-        def pagination_meta(paginated_records)
-          {
-            current_page: paginated_records.current_page,
-            next_page: paginated_records.next_page,
-            prev_page: paginated_records.prev_page,
-            total_pages: paginated_records.total_pages,
-            total_count: paginated_records.total_count
-          }
         end
     end
   end
