56f05e1ae17cff3830708c91f18914fa190f4bbf
Pen Peen <ikegami1022.h@gmail.com>
2025-04-10 23:16:54 +0900

    投稿モデル、マイグレーション、スキーマ、ファクトリ、テストを追加しました。ユーザーとの関連付けを実装し、タイトルとコンテンツのバリデーションを追加しました。

---
+++ db/migrate/20250410140950_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.text :content
+      t.string :top_image
+      t.boolean :published
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_10_140127) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_10_140950) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "content"
+    t.string "top_image"
+    t.boolean "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -20,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_10_140127) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
