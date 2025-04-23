038d92923c7b26333381521ff8d1e3b6f874a2e9
Pen Peen <ikegami1022.h@gmail.com>
2025-04-24 08:46:15 +0900

    Revert "feat(graphql): 投稿関連のフィールドを削除し、クエリタイプを整理"
    This reverts commit 7a433a5a382247b50f392b91531e17fa100a27e5.

---
+++ app/graphql/types/pagination_type.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Types
+  class PaginationType < Types::BaseObject
+    field :total_count, Integer, null: true
+    field :limit_value, Integer, null: true
+    field :total_pages, Integer, null: true
+    field :current_page, Integer, null: true
+  end
+end
