47b84a4b0ba1958048d20e6dd26fd2f3d9a98eef
Pen Peen <ikegami1022.h@gmail.com>
2025-05-11 10:59:23 +0900

    feat(graphql): GraphQLスキーマにタイムアウト、深さ、複雑性制限を追加

---
+++ app/graphql/my_timeout.rb
@@ -0,0 +1,6 @@
+class MyTimeout < GraphQL::Schema::Timeout
+  def handle_timeout(error, query)
+    Rails.logger.warn("GraphQL Timeout: #{error.message}: #{query.query_string}")
+    # Bugsnag.notify(error, {query_string: query.query_string})
+  end
+end
