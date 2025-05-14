e9cc67bbfc81cea4e9f3b1598820dfd0b0dde257
Pen Peen <ikegami1022.h@gmail.com>
2025-05-14 13:01:03 +0900

    chore(graphql): current_userフィールドをクエリタイプから削除し、重複を解消

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,9 +6,7 @@ module Types
     field :my_posts, resolver: Resolvers::MyPostsResolver
     field :node, resolver: Resolvers::NodeResolver
     field :nodes, resolver: Resolvers::NodesResolver
-    field :current_user, resolver: Resolvers::CurrentUserResolver
     field :published_posts, resolver: Resolvers::PublishedPostsResolver
-    field :my_posts, resolver: Resolvers::MyPostsResolver
     field :published_post, resolver: Resolvers::PublishedPostResolver
     field :published_search_posts, resolver: Resolvers::PublishedSearchPostsResolver
   end
