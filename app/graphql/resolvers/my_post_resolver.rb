ce03d7e4d27d9a9a6b692599176f99259ab7bc7a
Pen Peen <ikegami1022.h@gmail.com>
2025-05-20 08:41:37 +0900

    fix(graphql): 投稿取得時にnullを許容する

--- app/graphql/resolvers/my_post_resolver.rb
+++ app/graphql/resolvers/my_post_resolver.rb
@@ -16,20 +16,14 @@ query{
 
 module Resolvers
   class MyPostResolver < LoginRequiredResolver
-    type Types::PostType, null: false
+    type Types::PostType, null: true
     description "Fetches a post created by the current user"
 
     argument :id, ID, required: true
 
     def resolve(id:)
       current_user = context[:current_user]
-      post = current_user.posts.find_by(id:)
-
-      if post
-        post
-      else
-        raise GraphQL::ExecutionError, "Post not found"
-      end
+      current_user.posts.find_by(id:)
     end
   end
 end
