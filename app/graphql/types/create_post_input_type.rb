720939e2c4dd731fb1a985c468093e53276a797f
Pen Peen <ikegami1022.h@gmail.com>
2025-05-23 22:41:40 +0900

    feat(graphql): 投稿作成ミューテーションと関連サービスを追加

---
+++ app/graphql/types/create_post_input_type.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Types
+  class CreatePostInputType < Types::BaseInputObject
+    graphql_name "CreatePostInputType"
+    description "Attributes for creating a post"
+
+    argument :content, String, required: true, description: "記事内容"
+    argument :published, Boolean, required: true, description: "公開状態"
+    argument :thumbnail, String, required: false, description: "Base64エンコードされたサムネイル画像"
+    argument :title, String, required: true, description: "記事タイトル"
+  end
+end
