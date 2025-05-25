371ca848c86d6288be74e928cc94564a77831f27
Pen Peen <ikegami1022.h@gmail.com>
2025-05-25 19:48:42 +0900

    feat(graphql): スキーマ定義に型情報を追加することでコードの責務を明確化した。

--- app/graphql/types/published_type.rb
+++ app/graphql/types/published_type.rb
@@ -4,8 +4,8 @@ module Types
   class PublishedType < Types::BaseObject
     description "Published posts related fields"
 
-    field :post, resolver: Resolvers::PublishedPostResolver
-    field :posts, resolver: Resolvers::PublishedPostsResolver
-    field :search_posts, resolver: Resolvers::PublishedSearchPostsResolver
+    field :post, Types::PostType, null: true, resolver: Resolvers::PublishedPostResolver
+    field :posts, [Types::PostType], null: false, resolver: Resolvers::PublishedPostsResolver
+    field :search_posts, [Types::PostType], null: false, resolver: Resolvers::PublishedSearchPostsResolver
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,9 +2,9 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :current_user, resolver: Resolvers::CurrentUserResolver
-    field :my_post, resolver: Resolvers::MyPostResolver
-    field :my_posts, resolver: Resolvers::MyPostsResolver
+    field :current_user, Types::UserType, null: true, resolver: Resolvers::CurrentUserResolver
+    field :my_post, Types::PostType, null: true, resolver: Resolvers::MyPostResolver
+    field :my_posts, [Types::PostType], null: false, resolver: Resolvers::MyPostsResolver
     field :node, resolver: Resolvers::NodeResolver
     field :nodes, resolver: Resolvers::NodesResolver
     field :published, Types::PublishedType, resolver: Resolvers::PublishedResolver
