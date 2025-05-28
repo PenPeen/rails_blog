6c1f13fbba803bf755c980d3bc237a72889b1d97
Pen Peen <ikegami1022.h@gmail.com>
2025-05-28 17:25:05 +0900

    refactor(graphql): ミューテーションの順序を整理

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,13 +4,13 @@ module Types
   class MutationType < Types::BaseObject
     description 'MutationType'
 
+    field :confirm_registration, mutation: Mutations::Auth::ConfirmRegistrationMutation
     field :login, mutation: Mutations::Auth::LoginMutation
     field :logout, mutation: Mutations::Auth::LogoutMutation
-    field :confirm_registration, mutation: Mutations::Auth::ConfirmRegistrationMutation
 
     field :create_post, mutation: Mutations::Post::CreatePostMutation
-    field :update_post, mutation: Mutations::Post::UpdatePostMutation
     field :delete_post, mutation: Mutations::Post::DeletePostMutation
+    field :update_post, mutation: Mutations::Post::UpdatePostMutation
 
     field :create_user, mutation: Mutations::User::CreateUserMutation
     field :update_user_profile, mutation: Mutations::User::UpdateUserProfileMutation
